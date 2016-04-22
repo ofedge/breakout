aaf09e21789b282153360a3d5c56950ceab5adb6
liuyadong <liuyadong@chinatcc.com>
2016-04-22 15:47:08 +0800

    circle done!

--- mc/circle.js
+++ mc/circle.js
@@ -7,6 +7,7 @@ var len = 600;
 // 分割成多少份
 var $divide = document.getElementById('divide');
 var divide = $divide.value;
+
 $divide.onchange = function(){
 	draw(len, this.value);
 }
@@ -22,10 +23,16 @@ draw(len, divide);
  *            分割成多少份
  */
 function draw(len, divide) {
+	// 清除区域内所有图形
 	ctx.clearRect(0, 0, len, len);
-	drawCircle(len);
+	// 得到正方形数组
 	var squares = getSquares(len, divide);
-	drawLines(squares);
+	// 给正方形分组
+	groupSquares(squares, len);
+	// 画正方形
+	drawLines(len, squares);
+	// 画圆
+	drawCircle(len);
 }
 
 
@@ -48,12 +55,62 @@ function getSquares(len, divide) {
 						x: i * length,
 						y: j * length
 					},
-					sideLength: length,
-					quadrant: 0 // 象限, 顺时针开始,右上: 1, 右下: 2, 左下: 3, 左上: 4
+					sideLength: length, // 边长
+					quadrant: 0 // 位置, 顺时针开始,右上: 1, 右下: 2, 左下: 3, 左上: 4
 			};
 		}
 	}
 	return squares;
+}
+
+/**
+ * 给正方形按照位置数组分组
+ * 
+ * @param squares
+ *            正方形数组
+ * @param areaWidth
+ *            区域宽度
+ */
+function groupSquares(squares, areaWidth) {
+	var halfWidth = areaWidth / 2; // 区域半边长
+	for (var i = 0; i < squares.length; i++) {
+		for (var j = 0; j < squares[i].length; j++) {
+			var square = squares[i][j];
+			if (square.leftTop.x < halfWidth && square.leftTop.y < halfWidth){
+				square.quadrant = 1;
+				square.rightBottom = {
+					x: square.leftTop.x + square.sideLength,
+					y: square.leftTop.y + square.sideLength
+				}
+			} else if (square.leftTop.x >= halfWidth && square.leftTop.x < areaWidth && square.leftTop.y < halfWidth) {
+				square.quadrant = 2;
+				square.rightTop = {
+					x: square.leftTop.x + square.sideLength,
+					y: square.leftTop.y
+				}
+				square.leftBottom = {
+					x: square.leftTop.x,
+					y: square.leftTop.y + square.sideLength
+				}
+			} else if (square.leftTop.x >= halfWidth & square.leftTop.x < areaWidth && square.leftTop.y >= halfWidth && square.leftTop.y < areaWidth) {
+				square.quadrant = 3;
+				square.rightBottom = {
+					x: square.leftTop.x + square.sideLength,
+					y: square.leftTop.y + square.sideLength
+				}
+			} else if (square.leftTop.x < halfWidth && square.leftTop.y >= halfWidth && square.leftTop.y < areaWidth) {
+				square.quadrant = 4;
+				square.rightTop = {
+					x: square.leftTop.x + square.sideLength,
+					y: square.leftTop.y
+				}
+				square.leftBottom = {
+					x: square.leftTop.x,
+					y: square.leftTop.y + square.sideLength
+				}
+			}
+		}
+	}
 }
 
 
@@ -79,6 +136,8 @@ function drawCircle(areaWidth) {
 /**
  * 绘制所有的正方形
  * 
+ * @param areaWidith
+ * 	          区域宽度
  * @param squares
  *            存放所有正方形的数组
  * <br><br>
@@ -88,13 +147,50 @@ function drawCircle(areaWidth) {
  * width: 矩形宽度, 像素<br>
  * height: 矩形高度, 像素<br>
  */
-function drawLines(squares) {
+function drawLines(areaWidth, squares) {
+	var radius = areaWidth / 2;
+	var center = {x: radius, y: radius};
 	ctx.beginPath();
 	for (var i = 0; i < squares.length; i++) {
 		for (var j = 0; j < squares[i].length; j++) {
 			var square = squares[i][j];
+			if (square.quadrant == 1) {
+				if (calcDistance(square.leftTop, center) >= radius && calcDistance(square.rightBottom, center) <= radius) {
+					ctx.fillStyle = '#0000ff';
+					ctx.fillRect(square.leftTop.x, square.leftTop.y, square.sideLength, square.sideLength);
+				}
+			} else if (square.quadrant == 2) {
+				if (calcDistance(square.rightTop, center) >= radius && calcDistance(square.leftBottom, center) <= radius) {
+					ctx.fillStyle = '#0000ff';
+					ctx.fillRect(square.leftTop.x, square.leftTop.y, square.sideLength, square.sideLength);
+				}
+			} else if (square.quadrant == 3) {
+				if (calcDistance(square.rightBottom, center) >= radius && calcDistance(square.leftTop, center) <= radius) {
+					ctx.fillStyle = '#0000ff';
+					ctx.fillRect(square.leftTop.x, square.leftTop.y, square.sideLength, square.sideLength);
+				}
+			} else if (square.quadrant == 4) {
+				if (calcDistance(square.leftBottom, center) >= radius && calcDistance(square.rightTop, center) <= radius) {
+					ctx.fillStyle = '#0000ff';
+					ctx.fillRect(square.leftTop.x, square.leftTop.y, square.sideLength, square.sideLength);
+				}
+			}
 			ctx.strokeRect(square.leftTop.x, square.leftTop.y, square.sideLength, square.sideLength);
 		}
 	}
 	ctx.stroke();
+}
+
+/**
+ * 计算两个点的最短距离
+ * 
+ * @param a
+ *            点a
+ * @param b
+ *            点b
+ */
+function calcDistance(a, b) {
+	var i = Math.abs(a.x - b.x);
+	var j = Math.abs(a.y - b.y);
+	return Math.sqrt(i*i + j*j);
 }
